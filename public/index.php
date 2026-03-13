26622a10c166c0ad604a9e17e2d23bc96d7bcff3
zebastianahlin-art <zebastianahlin@gmail.com>
2026-03-13 22:47:54 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,33 @@
+<?php
 
+declare(strict_types=1);
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+define('BASE_PATH', dirname(__DIR__));
+
+require_once BASE_PATH . '/app/Core/App.php';
+require_once BASE_PATH . '/app/Core/Router.php';
+require_once BASE_PATH . '/app/Core/Controller.php';
+require_once BASE_PATH . '/app/Core/Database.php';
+require_once BASE_PATH . '/app/Core/View.php';
+
+require_once BASE_PATH . '/app/Controllers/HomeController.php';
+require_once BASE_PATH . '/app/Controllers/HostController.php';
+require_once BASE_PATH . '/app/Controllers/JoinController.php';
+
+$config = [
+    'app' => require BASE_PATH . '/config/app.php',
+    'database' => require BASE_PATH . '/config/database.php',
+];
+
+$app = new App($config);
+
+$router = $app->router();
+
+$router->get('/', ['HomeController', 'index']);
+$router->get('/host', ['HostController', 'index']);
+$router->get('/join', ['JoinController', 'index']);
+
+$router->dispatch($_SERVER['REQUEST_URI'] ?? '/', $_SERVER['REQUEST_METHOD'] ?? 'GET');
