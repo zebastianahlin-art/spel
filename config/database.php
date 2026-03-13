0666851f2d4fb9aa4af686cf713bc106f0a3b8e9
zebastianahlin-art <zebastianahlin@gmail.com>
2026-03-13 22:54:10 +0100

    Add database configuration for MySQL connection

--- config/database.php
+++ config/database.php
@@ -1,1 +1,13 @@
+<?php
 
+declare(strict_types=1);
+
+return [
+    'driver' => 'mysql',
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'database' => 'spel',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4',
+];
