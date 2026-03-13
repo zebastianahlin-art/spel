19b49ca9f1bfd660bcaf945dee1e2378c1f9e505
zebastianahlin-art <zebastianahlin@gmail.com>
2026-03-13 22:50:54 +0100

    Add HostController with index method

--- app/Controllers/HostController.php
+++ app/Controllers/HostController.php
@@ -1,1 +1,13 @@
+<?php
 
+declare(strict_types=1);
+
+final class HostController extends Controller
+{
+    public function index(): void
+    {
+        $this->view('host/index', [
+            'pageTitle' => 'Starta spel',
+        ]);
+    }
+}
