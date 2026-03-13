0b56d1e625be93b0af9c5ddc12dba08dbe9df647
zebastianahlin-art <zebastianahlin@gmail.com>
2026-03-13 22:53:46 +0100

    Add application configuration for Familjespel

--- config/app.php
+++ config/app.php
@@ -1,1 +1,11 @@
+<?php
 
+declare(strict_types=1);
+
+return [
+    'name' => 'Familjespel',
+    'env' => 'local',
+    'debug' => true,
+    'url' => 'http://localhost',
+    'timezone' => 'Europe/Stockholm',
+];
