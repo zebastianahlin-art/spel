efb169f3712cc70f1c2bc92068e840767cf9b306
zebastianahlin-art <zebastianahlin@gmail.com>
2026-03-13 22:50:35 +0100

    Add HomeController with index method

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,1 +1,16 @@
+<?php
 
+declare(strict_types=1);
+
+final class HomeController extends Controller
+{
+    public function index(): void
+    {
+        $appConfig = $this->app->config('app');
+
+        $this->view('home/index', [
+            'pageTitle' => 'Familjespel',
+            'appName' => $appConfig['name'] ?? 'Familjespel',
+        ]);
+    }
+}
