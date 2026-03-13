a5ef1c02ba991154226b59f6a6b7e0571b8a09aa
zebastianahlin-art <zebastianahlin@gmail.com>
2026-03-13 22:51:15 +0100

    Add JoinController with index method

--- app/Controllers/JoinController.php
+++ app/Controllers/JoinController.php
@@ -1,1 +1,13 @@
+<?php
 
+declare(strict_types=1);
+
+final class JoinController extends Controller
+{
+    public function index(): void
+    {
+        $this->view('join/index', [
+            'pageTitle' => 'Anslut till spel',
+        ]);
+    }
+}
