db588439ef11070e019ef8b19af332a0bbe55ec9
zebastianahlin-art <zebastianahlin@gmail.com>
2026-03-13 22:49:28 +0100

    Add abstract Controller class with view method

--- app/     Core/Controller.php
+++ app/     Core/Controller.php
@@ -1,1 +1,18 @@
+<?php
 
+declare(strict_types=1);
+
+abstract class Controller
+{
+    protected App $app;
+
+    public function __construct(App $app)
+    {
+        $this->app = $app;
+    }
+
+    protected function view(string $view, array $data = [], string $layout = 'layouts/main'): void
+    {
+        View::render($view, $data, $layout);
+    }
+}
