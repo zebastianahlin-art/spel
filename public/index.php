7fcf667d04b508b3e2253581b01a2c8ac52ab068
zebastianahlin-art <zebastianahlin@gmail.com>
2026-03-13 23:02:20 +0100

    Add Player and API routes to index.php

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
+session_start();
+
 define('BASE_PATH', dirname(__DIR__));
 
 require_once BASE_PATH . '/app/Core/App.php';
@@ -13,9 +15,14 @@ require_once BASE_PATH . '/app/Core/Controller.php';
 require_once BASE_PATH . '/app/Core/Database.php';
 require_once BASE_PATH . '/app/Core/View.php';
 
+require_once BASE_PATH . '/app/Repositories/RoomRepository.php';
+require_once BASE_PATH . '/app/Repositories/PlayerRepository.php';
+
 require_once BASE_PATH . '/app/Controllers/HomeController.php';
 require_once BASE_PATH . '/app/Controllers/HostController.php';
 require_once BASE_PATH . '/app/Controllers/JoinController.php';
+require_once BASE_PATH . '/app/Controllers/PlayerController.php';
+require_once BASE_PATH . '/app/Controllers/ApiController.php';
 
 $config = [
     'app' => require BASE_PATH . '/config/app.php',
@@ -27,7 +34,16 @@ $app = new App($config);
 $router = $app->router();
 
 $router->get('/', ['HomeController', 'index']);
+
 $router->get('/host', ['HostController', 'index']);
+$router->post('/host/create', ['HostController', 'create']);
+$router->get('/host/lobby', ['HostController', 'lobby']);
+
 $router->get('/join', ['JoinController', 'index']);
+$router->post('/join', ['JoinController', 'join']);
+
+$router->get('/player/lobby', ['PlayerController', 'lobby']);
+
+$router->get('/api/lobby', ['ApiController', 'lobby']);
 
 $router->dispatch($_SERVER['REQUEST_URI'] ?? '/', $_SERVER['REQUEST_METHOD'] ?? 'GET');
